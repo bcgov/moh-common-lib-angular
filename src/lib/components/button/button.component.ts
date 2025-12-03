c4a43f9a63657788d7a61e75365a06f125843b0c
Isaac Stevens <Isaac.stevens@pbcsolutions.ca>
2025-12-03 08:57:48 -0800

    added prettier

--- src/lib/components/button/button.component.ts
+++ src/lib/components/button/button.component.ts
@@ -1,5 +1,12 @@
 // TODO: Code refactor
-import { Component, ViewChild, EventEmitter, Input, Output, ElementRef } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  EventEmitter,
+  Input,
+  Output,
+  ElementRef,
+} from '@angular/core';
 
 /**
  * Button Component is a button which can be used across the application to have
@@ -28,9 +35,7 @@ import { Component, ViewChild, EventEmitter, Input, Output, ElementRef } from '@
   //   { provide: ControlContainer, useExisting: forwardRef(() => NgForm ) }
   // ]
 })
-
-export class ButtonComponent  {
-
+export class ButtonComponent {
   // Can pass the Style class of a button e.g. For primary, btn btn-primary. Default, btn btn-default. Error, btn btn-danger
   @Input() buttonType: 'default' | 'primary' | 'secondary' = 'default'; // Button style type
   @Input() disabled = false; // Disable button
@@ -38,11 +43,10 @@ export class ButtonComponent  {
   @Input() classNames = ''; // Additional classes for customization
   @Output() btnClick: EventEmitter<Event> = new EventEmitter<Event>();
   @ViewChild('button')
-    button!: ElementRef;
+  button!: ElementRef;
 
-       // Emit click event to parent component
-       onClick($event: Event) {
-        this.btnClick.emit($event);
-       
-     }  
+    // Emit click event to parent component
+     onClick($event: Event) {
+     this.btnClick.emit($event);
+  }
 }
